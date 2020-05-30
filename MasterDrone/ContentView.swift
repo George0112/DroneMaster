12335a5848c7c6b570fc8530e04b737142d37003
Chaowen <chaowen.nthu@gmail.com>
2020-05-30 15:27:18 +0800

    Initial Commit

---
+++ MasterDrone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MasterDrone
+//
+//  Created by 陳昭文 on 2020/5/30.
+//  Copyright © 2020 chaowen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
